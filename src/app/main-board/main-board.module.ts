c3e12d7cb56d8de4816faab9b9340cefa176ffd7
Nguyen Thai Vinh <thaivinh.nguyen@finastra.com>
2020-10-12 14:37:18 +0200

    add delete confir button

--- src/app/main-board/main-board.module.ts
+++ src/app/main-board/main-board.module.ts
@@ -9,6 +9,7 @@ import '@material/mwc-list/mwc-list-item';
 import '@cff/webcomponents/components/search-input/search-input.component';
 import '@cff/webcomponents/components/dashboard-layout/dashboard-layout.component';
 import '@cff/webcomponents/components/dashboard-item/dashboard-item.component';
+import '@cff/webcomponents/components/delete-confirmation/delete-confirmation.component';
 import { StickySidenavDirective } from './sticky-sidenav.directive';
 import { ExtensionInjectDirective } from './extension-inject.directive';
 
