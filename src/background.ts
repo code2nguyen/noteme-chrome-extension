d258c90bd89caad16b9d34fda1de8edd6d69692c
Nguyen Thai Vinh <thaivinh.nguyen@finastra.com>
2020-10-13 13:36:18 +0200

    clean background script

--- src/background.ts
+++ src/background.ts
@@ -1,36 +1,36 @@
-import Fuse from 'fuse.js';
-import { IndexableItemTypes, getText } from './app/services/utils';
+// import Fuse from 'fuse.js';
+// import { IndexableItemTypes, getText } from './app/services/utils';
 
-chrome.runtime.onInstalled.addListener(() => {
-  // indexDB();
-});
-
-chrome.runtime.onStartup.addListener(() => {
-  // indexDB();
-});
-
-function indexDB(): void {
-  // tslint:disable-next-line: typedef
-  chrome.storage.local.get(null, function (items) {
-    const docs = [];
-    for (const key in items) {
-      if (key.startsWith('ITEM_DATA__') && items[key]) {
-        const dataItem = JSON.parse(items[key]);
-        if (IndexableItemTypes.includes(dataItem.dataType)) {
-          docs.push({ id: dataItem.id, text: getText(dataItem.data, dataItem.dataType) });
-        }
-      }
-    }
-    const fuse = Fuse.createIndex(['text'], docs);
-    chrome.storage.local.set({ _ITEM_DATA_INDEX: JSON.stringify(fuse.toJSON()) });
-  });
-}
-
-// function syncChromeSyncToLocal() {
-// chrome.storage.sync.get(null, (remoteItems) => {
-//   chrome.storage.local.get(null, (localItems) => {
-//     // console.log(remoteItems, localItems);
-//     // TODO
-//   });
+// chrome.runtime.onInstalled.addListener(() => {
+//   indexDB();
 // });
+
+// chrome.runtime.onStartup.addListener(() => {
+//   indexDB();
+// });
+
+// function indexDB(): void {
+//   // tslint:disable-next-line: typedef
+//   chrome.storage.local.get(null, function (items) {
+//     const docs = [];
+//     for (const key in items) {
+//       if (key.startsWith('ITEM_DATA__') && items[key]) {
+//         const dataItem = JSON.parse(items[key]);
+//         if (IndexableItemTypes.includes(dataItem.dataType)) {
+//           docs.push({ id: dataItem.id, text: getText(dataItem.data, dataItem.dataType) });
+//         }
+//       }
+//     }
+//     const fuse = Fuse.createIndex(['text'], docs);
+//     chrome.storage.local.set({ _ITEM_DATA_INDEX: JSON.stringify(fuse.toJSON()) });
+//   });
 // }
+
+// // function syncChromeSyncToLocal() {
+// // chrome.storage.sync.get(null, (remoteItems) => {
+// //   chrome.storage.local.get(null, (localItems) => {
+// //     // console.log(remoteItems, localItems);
+// //     // TODO
+// //   });
+// // });
+// // }
